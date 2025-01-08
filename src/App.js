3a35ac11e84d642237296701e4952fde56c014f0
jong_min <whdals0525@gmail.com>
2025-01-08 13:00:18 +0900

    test

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <main>추운 겨울이 지나고 나면 따뜻한 봄이 찾아 올 것입니다.</main>;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const myApp = <h1>리액트 테스트</h1>;
+const root = ReactDOM.createRoot(document.getElementById("linecom"));
+root.render(myApp);
